3f7571c7a4e7b2618138e7633c2056ed81a1633e
fumiya <altema1217bbfumi@live.jp>
2021-08-21 06:51:08 +0900

    Update enum

---
+++ src/enum.ts
@@ -0,0 +1,22 @@
+export {}
+
+enum Months {
+  January,
+  February,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December,
+}
+
+Months.February
+
+console.log(Months.July)
+console.log(Months)
+console.log(Months[0])
