38a8c0ccb69c54386fefd56eb53ef8d7fdd8c2ab
fumiya <altema1217bbfumi@live.jp>
2021-08-20 17:30:55 +0900

    Update type

--- src/alias.ts
+++ src/alias.ts
@@ -3,3 +3,17 @@ export {}
 type Mojiretsu = String
 
 const fooString: Mojiretsu = 'test'
+
+type Profile = {
+  name: 'test'
+  age: 40
+}
+
+const example1: Profile = {
+  name: 'test',
+  age: 40,
+}
+
+type Profile2 = typeof example1
+
+console.log({ example1 })
