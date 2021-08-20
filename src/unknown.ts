78408b84a12993ee09a13af6f8e3d8de2f36a77c
fumiya <altema1217bbfumi@live.jp>
2021-08-20 17:45:45 +0900

    Update

--- src/unknown.ts
+++ src/unknown.ts
@@ -8,4 +8,8 @@ let numberAny: any = kansu()
 let numberUnknown: unknown = kansu()
 
 let sumAny = numberAny + 10
-let sumUnknown = numberUnknown + 10
+console.log(typeof numberUnknown)
+if (typeof numberUnknown === 'number') {
+  let sumUnknown = numberUnknown + 10
+  console.log({ sumUnknown })
+}
