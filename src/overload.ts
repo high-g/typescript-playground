479cef393b96ba6249a870ab126b268b09271905
fumiya <altema1217bbfumi@live.jp>
2021-08-21 16:56:49 +0900

    Update overload

--- src/overload.ts
+++ src/overload.ts
@@ -6,10 +6,11 @@ function double(value: string): string
 function double(value: any): any {
   if (typeof value === 'number') {
     return value * 2
-  } else if (typeof value === 'string') {
+  } else {
     return value + value
   }
 }
 
 console.log(double(100))
 console.log(double('go'))
+// console.log(double(true))
