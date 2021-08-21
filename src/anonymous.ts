ee0c6e684b079171086e111e91e596565edc7d37
fumiya <altema1217bbfumi@live.jp>
2021-08-21 15:55:23 +0900

    Update anonymous

---
+++ src/anonymous.ts
@@ -0,0 +1,10 @@
+export {}
+
+let bmi: (height: number, weight: number) => number = function (
+  height: number,
+  weight: number
+): number {
+  return weight / (((height / 100) * height) / 100)
+}
+
+console.log(bmi(168, 71))
