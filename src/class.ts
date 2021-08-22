8e0b578d3108003892b6ab596dc0d6907c70a7bd
fumiya <altema1217bbfumi@live.jp>
2021-08-22 12:11:34 +0900

    Update class

--- src/class.ts
+++ src/class.ts
@@ -1,6 +1,21 @@
 export {}
 
-class Person {}
+class Person {
+  name: string
+  age: number
 
-let taro = new Person()
-let hanako = new Person()
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+
+  profile(): string {
+    return `name: ${this.name}, age: ${this.age}`
+  }
+}
+
+let taro: Person = new Person('taro', 10)
+let hanako: Person = new Person('hanako', 11)
+
+console.log(taro.profile())
+console.log(hanako.profile())
