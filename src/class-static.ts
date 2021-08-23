a6d43bd7f8508c1e8b266e5b334f078d2765d166
fumiya <altema1217bbfumi@live.jp>
2021-08-23 09:11:04 +0900

    class static

--- src/class-static.ts
+++ src/class-static.ts
@@ -1,9 +1,13 @@
 export {}
-
 class Test {
   static isProgramer: boolean = true
   static firstName: string = 'Takeshi'
   static lastName: string = 'Test'
+
+  static work(): string {
+    return `wwwwwwwww${this.firstName}`
+  }
 }
 
 console.log(Test.firstName)
+console.log(Test.work())
