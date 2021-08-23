9f9e977309ef93fdc8b6cf6b63bde658f2a5c49b
fumiya <altema1217bbfumi@live.jp>
2021-08-23 12:19:00 +0900

    Update genelics

--- src/genelics.ts
+++ src/genelics.ts
@@ -15,3 +15,14 @@ const echo = <T>(arg: T): T => {
 console.log(echo<number>(100))
 console.log(echo<string>('hello'))
 console.log(echo<boolean>(true))
+
+class Mirror<T> {
+  constructor(public value: T) {}
+
+  echo(): T {
+    return this.value
+  }
+}
+
+console.log(new Mirror<number>(123).echo())
+console.log(new Mirror<string>('test').echo())
