c78fc79d64bbaeb235b90d4c86f93bb986b87b39
fumiya <altema1217bbfumi@live.jp>
2021-08-23 12:14:48 +0900

    Update generics

---
+++ src/genelics.ts
@@ -0,0 +1,17 @@
+export {}
+
+// const echo = (arg: number): number => {
+//   return arg
+// }
+
+// const echo = (arg: string): string => {
+//   return arg
+// }
+
+const echo = <T>(arg: T): T => {
+  return arg
+}
+
+console.log(echo<number>(100))
+console.log(echo<string>('hello'))
+console.log(echo<boolean>(true))
