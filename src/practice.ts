c95890da83bd3d331919ee645f97c51717dd7296
fumiya <altema1217bbfumi@live.jp>
2021-09-14 10:00:02 +0900

    update: omit

--- src/practice.ts
+++ src/practice.ts
@@ -16,3 +16,23 @@ const b: Required<Foo2> = {
   name: '11',
   age: 2,
 }
+
+type Foo3 = {
+  name?: string
+  age?: number
+}
+
+const c: Pick<Foo3, 'name'> = {
+  name: '1',
+}
+
+type Foo4 = {
+  name?: string
+  age?: number
+}
+
+const d: Omit<Foo4, 'age'> = {
+  name: '1',
+}
+
+const user = { name: 'kenji', age: 98 } as const
