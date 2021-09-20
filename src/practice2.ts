a5787d9d9e921209c8b6abb675327bad92bdfd7e
fumiya <altema1217bbfumi@live.jp>
2021-09-20 22:29:32 +0900

    refactor: Conditional Types

--- src/practice2.ts
+++ src/practice2.ts
@@ -34,3 +34,25 @@ const obj2: Something<flgObj> = {
     age: 11,
   },
 }
+
+// Conditional Types
+// T, U型の互換性があれば、X型、なければY型
+// T extends U ? X : Y
+interface Animal {
+  name: string
+  type: string
+}
+
+interface Dog {
+  name: string
+  type: string
+  age: number
+}
+
+type IsAnimal<T> = T extends Animal ? T : never
+
+const dog1: IsAnimal<Dog> = {
+  name: 'taro',
+  type: 'dog',
+  age: 11,
+}
