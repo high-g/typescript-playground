7124e1bdfdc78cdee772b1e114becf0de1e0da6d
fumiya <altema1217bbfumi@live.jp>
2021-09-20 22:12:26 +0900

    update: generics

--- src/practice2.ts
+++ src/practice2.ts
@@ -10,3 +10,26 @@ type res = ReturnType<Func>
 const test: res = 'sss'
 
 console.log(test)
+
+interface Something<T> {
+  id: number
+  flag: T
+}
+
+const obj1: Something<string> = {
+  id: 1,
+  flag: 'test',
+}
+
+type flgObj = {
+  name: string
+  age: number
+}
+
+const obj2: Something<flgObj> = {
+  id: 1,
+  flag: {
+    name: 'a',
+    age: 11,
+  },
+}
